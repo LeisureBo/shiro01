1290781eef7307980a0be928b71d597ec6d118d7
wangboc <wangboc@bj.xinwei.com.cn>
2017-09-19 19:59:18 +0800

    增加自定义多realm实现

---
+++ shiro01/src/main/java/com/bo/shiro/realm/MyRealmA.java
@@ -0,0 +1,55 @@
+package com.bo.shiro.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * @Description 自定义realm实现
+ * @author 王博
+ * @version 2017年9月19日　上午11:41:13
+ */
+public class MyRealmA implements Realm {
+
+	/**
+	 * 返回一个唯一的Realm名字
+	 */
+	@Override
+	public String getName() {
+		return this.getClass().getName();
+	}
+
+	/**
+	 * 判断此Realm是否支持此Token
+	 */
+	@Override
+	public boolean supports(AuthenticationToken token) {
+		// 仅支持UsernamePasswordToken类型的Token
+		return token instanceof UsernamePasswordToken;
+	}
+
+	/**
+	 * 根据Token获取认证信息
+	 */
+	@Override
+	public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		String principal = String.valueOf(token.getPrincipal());// 得到身份(用户名)
+		String credentials = new String((char[])token.getCredentials());// 得到认证/凭证(密码)
+//		System.out.println("principal : " + principal + ", credentials : " + credentials + "");
+		if (!"bo".equals(principal)) {
+			System.out.println("principal '" + principal + "' not match 'bo'.");
+			throw new UnknownAccountException("用户名/密码错误"); // 如果用户名错误
+		}
+		if (!"12345".equals(credentials)) {
+			System.out.println("credentials '" + credentials + "' not match '12345'.");
+			throw new IncorrectCredentialsException("用户凭证错误"); // 如果密码错误
+		}
+		// 如果身份认证验证成功，返回一个AuthenticationInfo实现；
+		return new SimpleAuthenticationInfo(principal, credentials, this.getName());
+	}
+}
--- shiro01/src/test/java/com/bo/shiro/test/HelloShiroTest.java
+++ shiro01/src/test/java/com/bo/shiro/test/HelloShiroTest.java
@@ -14,7 +14,10 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @Description shiro测试
@@ -23,12 +26,15 @@ import org.junit.Test;
  */
 public class HelloShiroTest {
 
+	private static Logger logger;
+	
 	/**
 	 * @Description 
 	 * @throws java.lang.Exception
 	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		logger = LoggerFactory.getLogger(HelloShiroTest.class);
 		// 加载log4j配置文件(相对路径:工程根目录)
 		PropertyConfigurator.configure("src/main/resources/config/log4j.properties"); 
 	}
@@ -58,10 +64,12 @@ public class HelloShiroTest {
 	}
 
 	/**
-	 * Test method for {@link com.bo.shiro.test.HelloShiro#HelloShiro()}.
+	 * 单个realm测试
 	 */
+	@SuppressWarnings("deprecation")
+	@Ignore
 	@Test
-	public void testHelloShiro() {
+	public void testAuthenticatorSingleRealm() {
 		// 通过new IniSecurityManagerFactory并指定一个ini配置文件来创建一个SecurityManager工厂
 		Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro/jdbc_realm.ini");
 		// 获取SecurityManager实例
@@ -74,20 +82,69 @@ public class HelloShiroTest {
 		// 创建token令牌:用户名/密码
 		UsernamePasswordToken token = new UsernamePasswordToken("shiro01","12345");
 		try {
-			// 调用subject.login方法进行登录，其会自动委托给SecurityManager.login方法进行登录
+			// 调用subject.login方法进行登录/身份验证，其会自动委托给SecurityManager.login方法进行登录
 			currentUser.login(token);
-			System.out.println("login success!");
+//			System.out.println("login success!");
 		} catch (AuthenticationException e) {
 			// 身份验证失败
+			logger.info("用户身份验证失败");
 			e.printStackTrace();
-			System.out.println("login failed!");
+//			System.out.println("login failed!");
 		}
+		
+		if (currentUser.isAuthenticated()) {
+	        logger.info("用户登录成功。");
+	    } else {
+	        logger.info("用户登录失败。");
+	    }
 		
 		// 断言用户已经登录
 		assertEquals("登录异常", true, currentUser.isAuthenticated());
 		
 		// 退出
 		currentUser.logout();
+	}
+	
+	/**
+	 * 多个自定义realm测试
+	 */
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testAuthenticatorMultiRealm(){
+		// 1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+	    Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro/custom_authenticator_multi_realm.ini");
+	     
+	    // 2、得到SecurityManager实例并绑定给SecurityUtils
+	    SecurityManager securityManager = factory.getInstance();
+	    SecurityUtils.setSecurityManager(securityManager);
+	     
+	    // 3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+	    Subject subject = SecurityUtils.getSubject();
+	    /* 
+	     * 用户身份Token 可能不仅仅是用户名/密码，也可能还有其他的，如登录时允许用户名/邮箱/手机号同时登录。
+	     */
+	    UsernamePasswordToken token = new UsernamePasswordToken("bo@163.com", "12345");
+	     
+	    try{
+	        // 4、登录，即身份验证
+	        subject.login(token);
+	    } catch (AuthenticationException e) {
+	        // 5、身份验证失败
+	        logger.info("用户身份验证失败");
+	        e.printStackTrace();
+	    }
+	     
+	    if (subject.isAuthenticated()) {
+	        logger.info("用户登录成功。");
+	    } else {
+	        logger.info("用户登录失败。");
+	    }
+	 
+	    // 断言用户已经登录
+	 	assertEquals("登录异常", true, subject.isAuthenticated());
+	    
+	    // 6、退出
+	    subject.logout();
 	}
 
 }
