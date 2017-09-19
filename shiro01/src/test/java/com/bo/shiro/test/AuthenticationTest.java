239db5783b457100d60ccd75a306c74816365313
LeisureBo <w.47@qq.com>
2017-09-19 23:09:47 +0800

    新增简单授权实例

--- shiro01/src/test/java/com/bo/shiro/test/AuthenticationTest.java
+++ shiro01/src/test/java/com/bo/shiro/test/AuthenticationTest.java
@@ -14,21 +14,27 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * @Description shiro身份验证测试 - JdbcRealm
+ * @Description shiro身份验证测试
  * @author 王博
  * @version 2017年9月14日　下午11:38:26
  */
 public class AuthenticationTest {
 
+	private static Logger logger;
+	
 	/**
 	 * @Description 
 	 * @throws java.lang.Exception
 	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		logger = LoggerFactory.getLogger(AuthenticationTest.class);
 		// 加载log4j配置文件(相对路径:工程根目录)
 		PropertyConfigurator.configure("src/main/resources/config/log4j.properties"); 
 	}
@@ -58,10 +64,12 @@ public class AuthenticationTest {
 	}
 
 	/**
-	 * Test method for {@link com.bo.shiro.test.HelloShiro#HelloShiro()}.
+	 * 单个realm测试
 	 */
+	@SuppressWarnings("deprecation")
+	@Ignore
 	@Test
-	public void testHelloShiro() {
+	public void testAuthenticatorSingleRealm() {
 		// 通过new IniSecurityManagerFactory并指定一个ini配置文件来创建一个SecurityManager工厂
 		Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro/jdbc_realm.ini");
 		// 获取SecurityManager实例
@@ -71,23 +79,72 @@ public class AuthenticationTest {
 		SecurityUtils.setSecurityManager(securityManager);
 		// 通过SecurityUtils得到Subject，其会自动绑定到当前线程
 		Subject currentUser = SecurityUtils.getSubject();
-		// 创建用户身份token令牌:用户名/密码，也可能还有其他的，如登录时允许用户名/邮箱/手机号同时登录
+		// 创建token令牌:用户名/密码
 		UsernamePasswordToken token = new UsernamePasswordToken("shiro01","12345");
 		try {
-			// 调用subject.login方法进行登录，其会自动委托给SecurityManager.login方法进行登录
+			// 调用subject.login方法进行登录/身份验证，其会自动委托给SecurityManager.login方法进行登录
 			currentUser.login(token);
-			System.out.println("login success!");
+//			System.out.println("login success!");
 		} catch (AuthenticationException e) {
 			// 身份验证失败
+			logger.info("用户身份验证失败");
 			e.printStackTrace();
-			System.out.println("login failed!");
+//			System.out.println("login failed!");
 		}
+		
+		if (currentUser.isAuthenticated()) {
+	        logger.info("用户登录成功。");
+	    } else {
+	        logger.info("用户登录失败。");
+	    }
 		
 		// 断言用户已经登录
 		assertEquals("登录异常", true, currentUser.isAuthenticated());
 		
 		// 退出
 		currentUser.logout();
+	}
+	
+	/**
+	 * 多个自定义realm测试
+	 */
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testAuthenticatorMultiRealm(){
+		// 1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+	    Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro/custom_authenticator_multi_realm.ini");
+	     
+	    // 2、得到SecurityManager实例并绑定给SecurityUtils
+	    SecurityManager securityManager = factory.getInstance();
+	    SecurityUtils.setSecurityManager(securityManager);
+	     
+	    // 3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+	    Subject subject = SecurityUtils.getSubject();
+	    /* 
+	     * 用户身份Token 可能不仅仅是用户名/密码，也可能还有其他的，如登录时允许用户名/邮箱/手机号同时登录。
+	     */
+	    UsernamePasswordToken token = new UsernamePasswordToken("bo@163.com", "12345");
+	     
+	    try{
+	        // 4、登录，即身份验证
+	        subject.login(token);
+	    } catch (AuthenticationException e) {
+	        // 5、身份验证失败
+	        logger.info("用户身份验证失败");
+	        e.printStackTrace();
+	    }
+	     
+	    if (subject.isAuthenticated()) {
+	        logger.info("用户登录成功。");
+	    } else {
+	        logger.info("用户登录失败。");
+	    }
+	 
+	    // 断言用户已经登录
+	 	assertEquals("登录异常", true, subject.isAuthenticated());
+	    
+	    // 6、退出
+	    subject.logout();
 	}
 
 }
---
+++ shiro01/src/test/java/com/bo/shiro/test/AuthorizationTest.java
@@ -0,0 +1,146 @@
+package com.bo.shiro.test;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Description shiro授权测试
+ * @author 王博
+ * @version 2017年9月19日　下午10:23:02
+ */
+public class AuthorizationTest {
+
+	private static Logger logger;
+	
+	/**
+	 * @Description 
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		logger = LoggerFactory.getLogger(AuthenticationTest.class);
+		// 加载log4j配置文件(相对路径:工程根目录)
+		PropertyConfigurator.configure("src/main/resources/config/log4j.properties"); 
+	}
+
+	/**
+	 * @Description 
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @Description 
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @Description 
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		// 1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+	    Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro/custom_authorize_permission.ini");
+	     
+	    // 2、得到SecurityManager实例并绑定给SecurityUtils
+	    SecurityManager securityManager = factory.getInstance();
+	    SecurityUtils.setSecurityManager(securityManager);
+	     
+	    // 3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+	    Subject subject = SecurityUtils.getSubject();
+	 
+	    UsernamePasswordToken token = new UsernamePasswordToken("bo", "12345");
+	    try{
+	        // 4、登录，即身份验证
+	        subject.login(token);
+	    } catch (AuthenticationException e) {
+	        // 5、身份验证失败
+	        logger.info("用户身份验证失败");
+	        e.printStackTrace();
+	    }
+	     
+	    // 用户身份得到确认
+	    if (subject.isAuthenticated()) {
+	        logger.info("用户登录成功。");
+	        /** 进行权限判断 */
+//	        this.testWhetherHasRole(subject);
+	        this.testWhetherHasPermission(subject);
+	    } else {
+	        logger.info("用户登录失败。");
+	    }
+	 
+	    // 6、退出
+	    subject.logout();
+	}
+	
+	/**
+	 * @Description 基于角色的访问控制
+	 * @param subject
+	 */
+	public void testWhetherHasRole(Subject subject){
+		/** Shiro提供了hasRole/isPermitted用于判断用户是否拥有某个角色/某个权限
+		 *	但是没有提供诸如hashAnyRole用于判断是否有某些角色(权限)中的某一个的方法 **/
+        // 判断拥有角色：role1
+        Assert.assertTrue("断言错误", subject.hasRole("role1"));
+        // 判断拥有角色：role1 and role2
+        Assert.assertTrue("断言错误", subject.hasAllRoles(Arrays.asList("role1", "role2")));
+        // 判断拥有角色：role1 and role2 and !role3
+        boolean[] result = subject.hasRoles(Arrays.asList("role1", "role2", "role3"));
+        Assert.assertEquals("断言错误", true, result[0]);
+        Assert.assertEquals("断言错误", true, result[1]);
+        Assert.assertEquals("断言错误", false, result[2]);
+        // 断言拥有角色：role1
+        subject.checkRole("role1");
+        // 断言拥有角色：role1 and role3 失败抛出异常
+        subject.checkRoles("role1", "role3");
+	}
+	
+	/**
+	 * @Description 基于权限的访问控制
+	 * @param subject
+	 */
+	public void testWhetherHasPermission(Subject subject){
+	        // 判断拥有权限：user:create
+	        Assert.assertTrue("断言错误", subject.isPermitted("user:create"));
+	        // 判断拥有权限：user:update and user:delete
+	        Assert.assertTrue("断言错误", subject.isPermittedAll("user:update", "user:delete"));
+	        // 判断没有权限：user:view
+	        Assert.assertFalse("断言错误", subject.isPermitted("user:view"));
+	         
+	        // 断言拥有权限：user:create
+	        subject.checkPermission("user:create");
+	        // 断言拥有权限：user:delete and user:update
+	        subject.checkPermissions("user:delete", "user:update");
+	        // 断言拥有权限：user:view 失败抛出异常
+	        subject.checkPermission("user:view");
+	}
+
+}
