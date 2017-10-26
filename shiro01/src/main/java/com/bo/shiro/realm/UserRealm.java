dd504bb3b56157ceef129a132bda0885b989e089
wangboc <wangboc@bj.xinwei.com.cn>
2017-10-26 20:05:15 +0800

    修补

--- shiro01/src/main/java/com/bo/shiro/realm/UserRealm.java
+++ shiro01/src/main/java/com/bo/shiro/realm/UserRealm.java
@@ -1,11 +1,26 @@
 package com.bo.shiro.realm;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+
+import com.bo.shiro.entity.Permission;
+import com.bo.shiro.entity.Role;
+import com.bo.shiro.entity.User;
+import com.bo.shiro.entity.UserStatus;
+import com.bo.shiro.service.UserService;
+import com.bo.shiro.service.UserServiceImpl;
 
 /**
  * @Description 
@@ -14,6 +29,8 @@ import org.apache.shiro.subject.PrincipalCollection;
  */
 public class UserRealm extends AuthorizingRealm {
 
+	private UserService userService = new UserServiceImpl();
+	
 	/**
 	 * @Description 
 	 * @param principals
@@ -21,7 +38,12 @@ public class UserRealm extends AuthorizingRealm {
 	 */
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-		return null;
+		String username = (String) principals.getPrimaryPrincipal();
+		SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+		Set<Role> roles = userService.findRoles(username);
+		authorizationInfo.setRoles(convertToStrs(userService.findRoles(username)));
+		authorizationInfo.setStringPermissions(convertToStrs(userService.findPermissions(username)));
+		return authorizationInfo;
 	}
 
 	/**
@@ -32,7 +54,39 @@ public class UserRealm extends AuthorizingRealm {
 	 */
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-		return null;
+		String username = (String) token.getPrincipal();
+		User user = userService.findByUsername(username);
+		if(user == null){
+			throw new UnknownAccountException();// 账户不存在
+		}
+		if(user.getUserStatus() == UserStatus.LOCKED){
+			throw new LockedAccountException();// 账户被锁定
+		}
+		//交给AuthenticatingRealm使用CredentialsMatcher进行密码匹配，如果觉得人家的不好可以在此判断或自定义实现
+		SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(
+				user.getUsername(),// 用户名
+				user.getPassword(),// 密码
+				ByteSource.Util.bytes(user.getCredentialsSalt()),// salt=username+salt
+				getName());
+		return authenticationInfo;
 	}
 
+	private Set<String> convertToStrs(Set origins){
+		Set<String> set = new HashSet<>();
+		if(origins != null && !origins.isEmpty()){
+			for(Object obj : origins){
+				if(obj instanceof Role){
+					Role role = (Role) obj;
+					set.add(role.getRole());
+				}else if(obj instanceof Permission){
+					Permission permission = (Permission) obj;
+					set.add(permission.getPermission());
+				}else {
+					break;
+				}
+			}
+		}
+		return set;
+	}
+	
 }
